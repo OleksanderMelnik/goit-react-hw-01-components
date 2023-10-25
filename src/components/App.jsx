61f0f1642fefb46acb6281854dfe430295b542d2
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-10-25 19:35:04 +0300

    to do Friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,9 @@
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
 
 import PropTypes from 'prop-types';
 import { Profile } from './Profile/Profile.js';
 import user from './Profile/user.json';
 import { Statistics } from './Statistics/Statistics.js';
 import data from './Statistics/data.json';
-
 
 export function App () {
   return (
