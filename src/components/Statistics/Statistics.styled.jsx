3f7ddd776138ba3d55730183b019680733912e4d
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-10-27 14:42:50 +0300

    to do css

---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled';
+
+export const generateColor = () =>
+  `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`
+
+export const StatisticsSection = styled.section`
+    margin-top: 50px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 302px;
+    padding: 60px 60px;
+    background-color: blanchedalmond;
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);`
+
+export const Title = styled.h2`
+    padding-top: 30px;
+    padding-bottom: 30px;
+    text-align: center;
+    text-transform: uppercase;
+    background-color: white;
+    font-weight: 700;
+    border-radius: 10px;`
+
+export const StatList = styled.ul`
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    justify-content: center;
+    padding: 0;`
+
+export const StatItem = styled.li`
+    padding: 10px 5px;
+    width: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;`
+
+export const Label = styled.span`
+    font-weight: 500;
+    font-size: 16px;`
+
+export const Percentage = styled.span`
+    font-weight: 700;
+    font-size: 18px;
+    margin-top: 5px;`
